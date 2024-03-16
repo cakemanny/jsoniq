97c1401be318ffb01a8a22d34e227389d7e92a4d
cakemanny <goldingd89@gmail.com>
2024-03-16 22:14:36 +0100

    add arrays to parser and handle keywords better

--- src/parse.rs
+++ src/parse.rs
@@ -1,15 +1,17 @@
 use nom::{
     branch::alt,
     bytes::complete::{escaped, tag, take_while},
-    character::complete::{alphanumeric1, char, multispace1, one_of},
-    combinator::{cut, map, map_opt, opt, value},
-    error::{context, Error},
-    multi::separated_list1,
+    character::complete::{
+        alphanumeric1, char, multispace0, multispace1, one_of, satisfy,
+    },
+    combinator::{cut, map, map_opt, value},
+    error::{context, make_error, ErrorKind, ParseError},
+    multi::{separated_list0, separated_list1},
     number::complete::double,
-    sequence::{delimited, preceded, separated_pair, terminated, tuple},
-    Err, IResult,
+    sequence::{delimited, preceded, terminated, tuple},
+    AsChar, Err, IResult,
 };
-use serde_json::{json, Number, Value};
+use serde_json::{Number, Value};
 
 use crate::ast::{CompOp, Expr, VarRef};
 
@@ -18,14 +20,30 @@ fn sp<'a>(i: &'a str) -> IResult<&'a str, &'a str> {
     take_while(move |c| chars.contains(c))(i)
 }
 
+/// Recognises a keyword
+///
+/// Fails if any alphanumeric characters follow the tag
+fn kw<'a, 'tag: 'a, E: ParseError<&'a str>>(
+    t: &'tag str,
+) -> impl Fn(&'a str) -> IResult<&'a str, &'a str, E> {
+    move |i: &'a str| {
+        let (remaining, matched) = tag(t)(i)?;
+
+        match satisfy::<_, &str, E>(|c| c.is_alphanum())(remaining) {
+            Ok(_) => Err(Err::Error(make_error(i, ErrorKind::Tag))),
+            Err(_) => Ok((remaining, matched)),
+        }
+    }
+}
+
 fn colon_eq<'a>(i: &'a str) -> IResult<&'a str, &'a str> {
     tag(":=")(i)
 }
 fn parse_comp_op<'a>(i: &'a str) -> IResult<&'a str, CompOp> {
     alt((
-        value(CompOp::EQ, tag("eq")),
+        value(CompOp::EQ, kw("eq")),
         // tag("ne"),
-        value(CompOp::LT, tag("lt")),
+        value(CompOp::LT, kw("lt")),
         //tag("le"),
         //tag("gt"),
         //tag("ge"),
@@ -37,16 +55,16 @@ fn parse_str<'a>(i: &'a str) -> IResult<&'a str, &'a str> {
 }
 
 fn parse_var_ref<'a>(i: &'a str) -> IResult<&'a str, &'a str> {
-    preceded(char('$'), alphanumeric1)(i)
+    context("variable reference", preceded(char('$'), alphanumeric1))(i)
 }
 
 fn boolean<'a>(input: &'a str) -> IResult<&'a str, bool> {
-    let parse_true = value(true, tag("true"));
-    let parse_false = value(false, tag("false"));
+    let parse_true = value(true, kw("true"));
+    let parse_false = value(false, kw("false"));
     alt((parse_true, parse_false))(input)
 }
 fn null<'a>(input: &'a str) -> IResult<&'a str, ()> {
-    value((), tag("null"))(input)
+    value((), kw("null"))(input)
 }
 
 fn string<'a>(i: &'a str) -> IResult<&'a str, &'a str> {
@@ -67,12 +85,56 @@ fn parse_literal<'a>(i: &'a str) -> IResult<&'a str, Value> {
     ))(i)
 }
 
+// Adapted from: https://gist.github.com/eignnx/3c8444b8e2f4d8ce10fcd97815f29d2e
+pub mod ws0 {
+    use super::*;
+
+    pub fn after<'a, F: 'a, O, E: ParseError<&'a str>>(
+        inner: F,
+    ) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+    where
+        F: Fn(&'a str) -> IResult<&'a str, O, E>,
+    {
+        terminated(inner, multispace0)
+    }
+
+    pub fn around<'a, F: 'a, O, E: ParseError<&'a str>>(
+        inner: F,
+    ) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+    where
+        F: Fn(&'a str) -> IResult<&'a str, O, E>,
+    {
+        delimited(multispace0, inner, multispace0)
+    }
+
+    pub fn before<'a, F: 'a, O, E: ParseError<&'a str>>(
+        inner: F,
+    ) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+    where
+        F: Fn(&'a str) -> IResult<&'a str, O, E>,
+    {
+        terminated(inner, multispace0)
+    }
+}
+
+fn parse_array(i: &str) -> IResult<&str, Vec<Expr>> {
+    // Should we cut, or will it conflict with array unboxing?
+    context(
+        "array",
+        delimited(
+            ws0::after(char('[')),
+            separated_list0(ws0::after(char(',')), ws0::after(parse_expr)),
+            char(']'),
+        ),
+    )(i)
+}
+
 fn parse_var_ref_in<'a>(i: &'a str) -> IResult<&'a str, (VarRef, Expr)> {
     let (remaining, (var_ref, _, _, _, expr)) = tuple((
         parse_var_ref,
         multispace1,
-        tag("in"),
-        multispace1,
+        kw("in"),
+        multispace0,
         parse_expr,
     ))(i)?;
     Ok((remaining, (var_ref.into(), expr)))
@@ -80,8 +142,8 @@ fn parse_var_ref_in<'a>(i: &'a str) -> IResult<&'a str, (VarRef, Expr)> {
 
 fn parse_for_line<'a>(i: &'a str) -> IResult<&'a str, Vec<(VarRef, Expr)>> {
     let (remaining, (_, _, ins)) = tuple((
-        tag("for"),
-        multispace1,
+        kw("for"),
+        multispace0,
         separated_list1(char(','), parse_var_ref_in),
     ))(i)?;
     Ok((remaining, ins))
@@ -90,7 +152,10 @@ fn parse_for_line<'a>(i: &'a str) -> IResult<&'a str, Vec<(VarRef, Expr)>> {
 fn parse_expr<'a>(i: &'a str) -> IResult<&'a str, Expr> {
     // ... | array | object | literal
     // TODO: rest
-    map(parse_literal, Expr::Literal)(i)
+    alt((
+        map(parse_array, Expr::Array),
+        map(parse_literal, Expr::Literal),
+    ))(i)
 }
 
 // fncall identified , terminated('(', many0(expr)  ')')
@@ -98,16 +163,59 @@ fn parse_expr<'a>(i: &'a str) -> IResult<&'a str, Expr> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use nom::error::ErrorKind;
+    use nom::error::{Error, ErrorKind};
     use serde_json::json;
+
+    #[test]
+    fn test_parse_bool() {
+        assert_eq!(boolean("true"), Ok(("", true)));
+        assert_eq!(boolean("true "), Ok((" ", true)));
+        assert_eq!(boolean("false"), Ok(("", false)));
+
+        assert_eq!(
+            boolean("truer"),
+            Err(Err::Error(Error::new("truer", ErrorKind::Tag)))
+        );
+        assert_eq!(
+            boolean("falsey"),
+            Err(Err::Error(Error::new("falsey", ErrorKind::Tag)))
+        );
+    }
 
     #[test]
     fn test_parse_str() {
         assert_eq!(parse_str("captains"), Ok(("", "captains")));
     }
+
     #[test]
     fn test_parse_null() {
         assert_eq!(null("null"), Ok(("", ())));
+
+        assert_eq!(
+            null("nuller"),
+            Err(Err::Error(Error::new("nuller", ErrorKind::Tag)))
+        );
+    }
+
+    #[test]
+    fn test_parse_array() {
+        assert_eq!(parse_array("[]"), Ok(("", vec![])));
+        assert_eq!(parse_array("[  ]"), Ok(("", vec![])));
+        assert_eq!(
+            parse_array("[1]"),
+            Ok(("", vec![Expr::Literal(json!(1.0))]))
+        );
+        assert_eq!(
+            parse_array("[ 1 ]"),
+            Ok(("", vec![Expr::Literal(json!(1.0))]))
+        );
+        assert_eq!(
+            parse_array("[ 1 , 2 ]"),
+            Ok((
+                "",
+                vec![Expr::Literal(json!(1.0)), Expr::Literal(json!(2.0))]
+            ))
+        );
     }
 
     #[test]
@@ -123,8 +231,16 @@ mod tests {
     #[test]
     fn test_parse_for_line() {
         assert_eq!(
-            parse_for_line("for $x in null"),
-            Ok(("", vec![(VarRef::from("x"), Expr::Literal(json!(null)))]))
+            parse_for_line("for $x in []"),
+            Ok(("", vec![(VarRef::from("x"), Expr::Array(vec![]))]))
+        );
+        assert_eq!(
+            parse_for_line("for$x in[]"),
+            Ok(("", vec![(VarRef::from("x"), Expr::Array(vec![]))]))
+        );
+        assert_eq!(
+            parse_for_line("for $x innull"),
+            Err(Err::Error(Error::new("innull", ErrorKind::Tag)))
         );
     }
 }
